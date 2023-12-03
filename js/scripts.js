fe30259e9049491a25bbeee66e3d3f73380f0f12
Rhonda Sheppard <rhondasheppard32@gmail.com>
2023-12-03 14:30:49 -0500

    Part 2

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+console.log('javascript connected!');
+    
+const carousel = new bootstrap.Carousel('#homeCarousel', {
+    interval: 5000,
+    pause: false
+})
+
+const carouselButton = document.getElementById('carouselButton');
+const faIcon = document.getElementById('faButton');
+
+carouselButton.addEventListener('click', function () {
+    if (faIcon.classList.contains('fa-pause')) {
+        faIcon.classList.remove('fa-pause');
+        faIcon.classList.add('fa-play');
+        carousel.pause();
+    } else {
+        faIcon.classList.remove('fa-play');
+        faIcon.classList.add('fa-pause');
+        carousel.cycle();
+    }
+})   
+
+
+
+// when the pause button is clicked, pause the carousel
+/*const carouselPause = document.getElementById('carouselPause');
+carouselPause.addEventListener('click', function() {
+    console.log('pausing the carousel');
+    carousel.pause();
+})
+
+// when the play button is clicked, begin cycling through the images
+const carouselPlay = document.getElementById('carouselPlay');
+carouselPlay.addEventListener('click', function() {
+    console.log('cycle the carousel');
+    carousel.cycle();
+}) */
